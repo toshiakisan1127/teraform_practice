e899875a396d2c299c65db04a853f520f1e62c2f
toshiakisan1127 <toshiakisan1127@gmail.com>
2022-02-13 01:44:32 +0900

    add alb

--- 8章/network.tf
+++ 8章/network.tf
@@ -134,3 +134,72 @@ module "example_sg" {
   port        = 80
   cidr_blocks = ["0.0.0.0/0"] # 全てのIPからのインバウンドを許可。意味なくね？
 }
+
+resource "aws_lb" "example" {
+  name                       = "example"
+  load_balancer_type         = "application"
+  internal                   = false
+  idle_timeout               = 60
+  enable_deletion_protection = true
+
+  subnets = [
+    aws_subnet.public_0.id,
+    aws_subnet.public_1.id
+  ]
+
+  access_logs {
+    bucket  = aws_s3_bucket.alb_log.id
+    enabled = true
+  }
+
+  security_groups = [
+    module.http_sg.security_group_id,
+    module.https_sg.security_group_id,
+    module.http_redirect_sg.security_group_id
+  ]
+}
+
+module "http_sg" {
+  source      = "./security_group"
+  name        = "http_sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source      = "./security_group"
+  name        = "https-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 443
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "http_redirect_sg" {
+  source      = "./security_group"
+  name        = "http-redirect-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 8080
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これは「HTTP」です"
+      status_code  = "200"
+    }
+  }
+}
+
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+}
