601880493d9ab25d4d7b9b70a27bacbcddb35209
toshiakisan1127 <toshiakisan1127@gmail.com>
2022-02-12 17:38:33 +0900

    add private subnet, nat gateway

--- 7章/network.tf
+++ 7章/network.tf
@@ -45,18 +45,39 @@ output "public_subnet_route_table_id" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id                          = aws_vpc.example.id
-  cidr_block                      = "10.0.64.0/24"
-  availability_zone               = "ap-northeast-1a"
-  map_customer_owned_ip_on_launch = false
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.64.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
 }
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.example.id
 }
 
-resource "aws_route_table_assosiation" "private" {
+resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
+}
+
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public.id
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  nat_gateway_id         = aws_nat_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
 }
 
