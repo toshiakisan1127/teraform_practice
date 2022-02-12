225824a98f048ac20d38e862e2a3df542603d5fc
toshiakisan1127 <toshiakisan1127@gmail.com>
2022-02-12 17:10:08 +0900

    パブリックサブネットを作成

--- 7章/network.tf
+++ 7章/network.tf
@@ -1,9 +1,43 @@
 resource "aws_vpc" "example" {
-  cidr_blocks          = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
     Name = "example"
   }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.example.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.public.id
 }
